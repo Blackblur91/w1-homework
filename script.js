d5a0bedeca9d748b19163b77aac92f966384e14e
Blackblur91 <feketebalazs04@gmail.com>
2023-09-26 20:04:12 +0200

    making multiple function

---
+++ script.js
@@ -0,0 +1,13 @@
+function logger(param) {
+  console.log(param)
+
+}
+logger("ez egy szorzás")
+
+function multipleThreeNumbers (x,y,z) {
+  let result = x*y*z
+  return result
+}
+
+logger(multipleThreeNumbers(3,4,5))
+
