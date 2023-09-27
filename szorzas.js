aa8df2c78924913451fc553a9e3d5c1eca0e6336
Blackblur91 <feketebalazs04@gmail.com>
2023-09-27 11:49:00 +0200

    new function

--- szorzas.js
+++ szorzas.js
@@ -1,13 +1,25 @@
-function logger(param) {
+function logger(szoveg) {
+  console.log(szoveg);
+}
+logger("Ez egy szorzás");
+
+function multipleThreeNumbers(x, y, z) {
+  let result = x * y * z;
+  return result;
+}
+logger(multipleThreeNumbers(8, 4, 5));
+
+
+function eletkor (param) {
   console.log(param)
 
 }
-logger("ez egy szorzás")
 
-function multipleThreeNumbers (x,y,z) {
-  let result = x*y*z
-  return result
+eletkor("Te " + ageNumber(2023, 1991) + " éves vagy." );
+
+function ageNumber(date, birth) {
+  let age = date - birth;
+  return (age);
 }
 
-logger(multipleThreeNumbers(3,4,5))
-
+logger(ageNumber(2023, 1991))
